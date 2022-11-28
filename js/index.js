daaa074a7f79dc972c2274b88b710dcd5b1ae218
Severin Weißflog <117283164+2Akuma2@users.noreply.github.com>
2022-11-28 14:18:52 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,6 +6,12 @@ try
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  
+  const paramTag = core.getInput('paramTag');
+  console.log(`Nice ${paramTag}!`);
+  const resolvedParamTag = getVersionWithHotfixWithoutPostfix(paramTag);
+  core.setOutput("resolvedParamTag", resolvedParamTag);
+  
   const time = (new Date()).toTimeString();
   core.setOutput("timee", time);
   // Get the JSON webhook payload for the event that triggered the workflow
