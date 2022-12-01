e52e83ba78394eb845e513e0c0ea805eaf33a991
Severin Weißflog <117283164+2Akuma2@users.noreply.github.com>
2022-12-01 15:44:16 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,9 +30,9 @@ try
   
   const test3 = spawnSync('echo', [`$(java -version)`], {shell: true, stdio: 'inherit'});
   
-  const test4 = spawnSync('echo', [`$(pwd)`], {shell: true, stdio: 'inherit'});
+  //const test4 = spawnSync('echo', [`$(pwd)`], {shell: true, stdio: 'inherit'});
   
-  const test5 = spawnSync('echo', [`$(cd ../)`], {shell: true, stdio: 'inherit'});
+  //const test5 = spawnSync('echo', [`$(cd ../)`], {shell: true, stdio: 'inherit'});
   
   const test6 = spawnSync('sudo sh -c', [`$(pwd) && $(cd ../) && $(pwd)`], {shell: true, stdio: 'inherit'});
 //////////////////////////////////////////////////////////////////////////
