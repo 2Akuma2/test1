86476318ce0a4ef3496d9d7e8746dd2f55c1d8f4
Severin Weißflog <117283164+2Akuma2@users.noreply.github.com>
2022-12-05 08:44:41 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ try
   //  console.log("Output: ", output.toString())
   //})
   
-  const test2 = spawnSync('echo', [`This is test2 with paramTag: ${paramTag}`], {stdio: 'inherit'});
+  const test2 = spawnSync('echo', [`This is test2 with paramTag: ${paramTag}`], {shell: true, stdio: 'inherit'});
   //test2.stdout.on('data', output => {
   //  // the output data is captured and printed in the callback
   //  console.log("Output: ", output.toString())
