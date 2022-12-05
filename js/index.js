ed37551331a68d8b3501a99a483eded86130eb1e
Severin Weißflog <117283164+2Akuma2@users.noreply.github.com>
2022-12-05 15:36:21 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -296,6 +296,15 @@ function getAditoMajorVersion()
   }
 }
 
+//////////////////////////////////////////////////////////////////////////
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+//////////////////////////////////////////////////////////////////////////
 
 function getPipelineVersion(type) // was static?
 {
