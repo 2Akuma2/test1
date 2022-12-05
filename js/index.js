424b5fb1ba362fe059bf2d564b68fbc0ca5028e1
Severin Weißflog <117283164+2Akuma2@users.noreply.github.com>
2022-12-05 16:02:56 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -278,7 +278,7 @@ function getAditoMajorVersion()
       }
       
       console.log("check in: " + JSON.stringify(pomResponse.pomObject) + " : checked in");
-      var majorVersion = pomResponse.pomObject['adito.version.external'];
+      var majorVersion = pomResponse.pomObject.properties['adito.version.external'];
       //console.log("MajorVersionCheckpoint 0 in: " + pomResponse.pomObject.properties.adito.version.external);
       console.log("MajorVersionCheckpoint in: " + majorVersion);
       return majorVersion;
